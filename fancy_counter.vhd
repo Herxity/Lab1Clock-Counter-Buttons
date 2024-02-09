f3a2f8d1cc941f26adac08d5d73d903d348786b0
Rube <34107608+Herxity@users.noreply.github.com>
2024-02-08 20:39:05 -0500

    Design srcs and tb

---
+++ fancy_counter.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/08/2024 06:42:46 PM
+-- Design Name: 
+-- Module Name: counter_top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity fancy_counter is
+--  Port ( );
+end fancy_counter;
+
+architecture Behavioral of fancy_counter is
+
+begin
+
+
+end Behavioral;
