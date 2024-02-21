e536bc617d6d085420c0a6eb15c9e8159a561535
Rube <34107608+Herxity@users.noreply.github.com>
2024-02-21 17:33:17 -0500

    Update debounce.vhd

--- debounce.vhd
+++ debounce.vhd
@@ -35,21 +35,24 @@ architecture Behavioral of debounce is
     signal counter : std_logic_vector(21 downto 0);
 begin
 
-    debounce: process(clk,btn)
+    debounce: process(clk)
     begin
-        if btn = '0' then
-            if rising_edge(clk)then
+        if rising_edge(clk)then
+            if btn = '1' then
+
                 -- Wait 20ms = wait 2.5million cycles
-                if (unsigned(counter) < 2499999) then
-                    counter <= std_logic_vector(unsigned(counter) + 1);
+                if (unsigned(counter) > 2499999) then
+                    dbnc <= '1';
+
                 else
-                    counter <= (others => '0');
-                    dbnc <= '1'; --Happens @ Reset
+                    counter <= std_logic_vector(unsigned(counter) + 1);
+
                 end if;
+
+            else
+                dbnc <='0';
+                counter <= (others => '0');
             end if;
-        else
-            dbnc <='0';
-            counter <= (others => '0');
         end if;
     end process;
 
